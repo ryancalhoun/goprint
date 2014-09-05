ed9b9fbf3b3e68a92452eeac0184a499d4c699bf
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2014-09-05 08:45:51 -0500

    Moved urls to top.

--- preview/open.js
+++ preview/open.js
@@ -1,3 +1,6 @@
+var server = "http://print.home:8080";
+var preview = "http://print.home/preview";
+
 (function(data) {
 	var request = function(method, url, onComplete, data) {
 		var req = new XMLHttpRequest();
@@ -9,19 +12,19 @@
 		req.send(data);
 	}
 	var onPageLoad = function(loc) {
-		request("POST", "http://print.home:8080/capture", function(res) {
+		request("POST", server + "/capture", function(res) {
 			if(res.status == 200)
 			{
 				file = res.getResponseHeader("X-Output-File");
-				loc.replace("http://print.home/preview/?file=" + file);
+				loc.replace(preview + "/?file=" + file);
 			}
 			else
 			{
-				loc.replace("http://print.home/preview/error.html");
+				loc.replace(preview + "/error.html");
 			}
 		}, data);
 	}
-	request("GET", "http://print.home/preview", function(res) {
+	request("GET", preview, function(res) {
 		if(res.status == 0)
 		{
 			alert("Print server is not running");
