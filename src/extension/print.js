bbcc0e5cd2636e326b0cd5560516fbe6b3c9d5e8
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2014-09-05 09:02:59 -0500

    move preview url to top

--- src/extension/print.js
+++ src/extension/print.js
@@ -1,3 +1,5 @@
+var preview = "http://print.home/preview";
+
 chrome.contextMenus.create({"title": "Print page", "contexts": ["page"], "onclick": function(info, tab) {
 	chrome.pageCapture.saveAsMHTML({tabId: tab.id}, function(data) {
 		var script = new XMLHttpRequest();
@@ -8,7 +10,7 @@ chrome.contextMenus.create({"title": "Print page", "contexts": ["page"], "onclic
 				fn(data);
 			}
 		}
-		script.open("GET", "http://print.home/preview/open.js?" + escape(new Date()), true);
+		script.open("GET", preview + "/open.js?" + escape(new Date()), true);
 		script.send();
 	});
 }});
